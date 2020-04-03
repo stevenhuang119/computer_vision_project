b9bd363dd20ad8678806a237951704850ed5af93
Steven Huang <lingxiaohuang@Stevens-MacBook-Pro-4.local>
2020-04-03 18:23:33 -0400

    followed the pytorch documentation to detect objects

--- helper.py
+++ helper.py
@@ -1,3 +1,12 @@
 """
 Helper functions for the other python scripts.
 """
+
+import numpy as np
+
+def img_transformation(img):
+    """
+    Transform the input image to the correct format
+    to be feed into the Faster RCNN.
+    """
+    return img
--- object_detection.py
+++ object_detection.py
@@ -4,6 +4,9 @@ A python module that implements human detection in an image.
 
 import numpy as np
 import torchvision
+from PIL import Image
+
+from helper import *
 
 class detector:
     """
@@ -28,13 +31,50 @@ class detector:
         Initializes the model.
         """
         self.model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
-        pass
+        self.model.eval()
+        self.CATEGORY_NAMES = [
+            '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+            'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
+            'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
+            'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'N/A',
+            'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+            'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
+            'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
+            'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+            'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table',
+            'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
+            'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A', 'book',
+            'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
+            ]
 
-    def forward(self):
+
+    def make_predictions(self, img_path, threshold=0.8):
         """
         Compute the objects detected and output a box around the objects.
         """
-        pass
+        # obtain the images
+        img = Image.open(img_path) 
+        img = img_transformation(img)
+
+        # run the image through the RCNN model
+        pred = self.model([img]) # only passing one image in for testing
+        pred_labels = [self.CATEGORY_NAMES[i] for i in list(
+            pred[0]['labels'].numpy()
+        )]
+        pred_boxes = [[i[0], i[1], i[2], i[3]] for i in list(
+            pred[0]['boxes'].detch().numpy()
+        )]
+        pred_score = list(pred[0]['scores'].detach().numpy())
+
+        # filter the predictions with scores higher than the threshold
+        pred_index_t = [pred_score.index(x) for x in pred_score if x>threshold][-1]
+        pred_boxes = None
+        pred_labels = None
+
+        return pred_boxes, pred_labels
+
+
+
 
     def evaluate(self):
         """
