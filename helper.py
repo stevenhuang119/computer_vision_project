50bcf0407928bbb380532f389aa7d872e530384e
Steven Huang <lingxiaohuang@Stevens-MacBook-Pro-4.local>
2020-04-03 21:43:41 -0400

    test image successful

--- helper.py
+++ helper.py
@@ -3,10 +3,12 @@ Helper functions for the other python scripts.
 """
 
 import numpy as np
+import torchvision.transforms as T
 
 def img_transformation(img):
     """
     Transform the input image to the correct format
     to be feed into the Faster RCNN.
     """
-    return img
+    transform = T.Compose([T.ToTensor()])
+    return transform(img)
--- object_detection.py
+++ object_detection.py
@@ -1,16 +1,16 @@
 """
 A python module that implements human detection in an image.
 """
-
-import numpy as np
-import torchvision
 from PIL import Image
+import torchvision
+from helper import img_transformation
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from cv2 import cv2
 
-from helper import *
-
-class detector:
+class Detector:
     """
-    Utilize pre-trained Faster R-CNN Resnet-50 Model
+    Utilize pre-trained Faster R-CNN Resnet-50 Model for object detection;
 
     Input to the pre-trained model:
     1) a tensor of dimension (n, c, h, w)
@@ -31,7 +31,9 @@ class detector:
         Initializes the model.
         """
         self.model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+        print('model evaluating')
         self.model.eval()
+        print('end of model evluation')
         self.CATEGORY_NAMES = [
             '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
             'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
@@ -48,7 +50,7 @@ class detector:
             ]
 
 
-    def make_predictions(self, img_path, threshold=0.8):
+    def make_predictions(self, img_path, threshold=0.5):
         """
         Compute the objects detected and output a box around the objects.
         """
@@ -57,27 +59,60 @@ class detector:
         img = img_transformation(img)
 
         # run the image through the RCNN model
+        print('----model predicting----')
         pred = self.model([img]) # only passing one image in for testing
+        print('----prediction finishes---')
         pred_labels = [self.CATEGORY_NAMES[i] for i in list(
             pred[0]['labels'].numpy()
         )]
         pred_boxes = [[i[0], i[1], i[2], i[3]] for i in list(
-            pred[0]['boxes'].detch().numpy()
+            pred[0]['boxes'].detach().numpy()
         )]
         pred_score = list(pred[0]['scores'].detach().numpy())
 
         # filter the predictions with scores higher than the threshold
         pred_index_t = [pred_score.index(x) for x in pred_score if x>threshold][-1]
-        pred_boxes = None
-        pred_labels = None
+        pred_boxes = pred_boxes[:pred_index_t+1]
+        pred_labels = pred_labels[:pred_index_t+1]
 
         return pred_boxes, pred_labels
 
 
+    def object_detection_pipeline(self, img_path, threshold=0.5,
+                                  rect_th=3, text_size=3, text_th=3):
+        """
+        Establish an entire pipeline where the image is taken in, processed
+        by the model, results being interpreted, and boxes drawn
+        """
+        boxes, labels = self.make_predictions(img_path, threshold)
+        img = cv2.imread(img_path)
+        cv2.imwrite('./justuploaded.png', img)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) 
+        cv2.imwrite('./converted.png', img)
+        for i in range(len(boxes)):
+            cv2.rectangle(img,
+                          (int(boxes[i][0]), int(boxes[i][1])),
+                          (int(boxes[i][2]), int(boxes[i][3])),
+                          (0,255,0),
+                          thickness=rect_th) # draw the boxese
+            cv2.putText(img, labels[i],
+                        (int(boxes[i][0]), int(boxes[i][1])),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        text_size, (0,255,0), thickness=text_th)
+        # plt.figure(figsize=(20,30))
+        # plt.imshow(img)
+        # plt.xticks([])
+        # plt.yticks([])
+        cv2.imwrite('./result.png', img)
 
 
-    def evaluate(self):
-        """
-        Evaluate the model with mAP.
-        """
-        pass
+
+def main():
+    detector = Detector()
+    detector.object_detection_pipeline('./girl_cars.jpg', rect_th=15, text_th=7,
+                                       text_size=5, threshold=0.9)
+
+
+
+if __name__ == "__main__":
+    main()
